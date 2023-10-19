77b5847a738949553085c0c54c60e5305c809856
Leon Schimmel <leonschimmel15@gmail.com>
2023-10-19 22:47:24 +0200

    Version 1.0.0.0

--- Common/NexusFont.Static.cs
+++ Common/NexusFont.Static.cs
@@ -7,6 +7,10 @@ public sealed partial record NexusFont
     /// </summary>
     /// <param name="width">The width the font should have</param>
     /// <param name="height">The height the font should have</param>
+    /// <remarks>
+    /// Keep in mind that some characters might not work in this font.<br/>
+    /// I would recommend to test if the character is included.
+    /// </remarks>
     /// <returns><see cref="NexusFont"/></returns>
     public static NexusFont Consolas(int width, int height)
         => new("Consolas", width, height);
@@ -16,6 +20,10 @@ public sealed partial record NexusFont
     /// </summary>
     /// <param name="width">The width the font should have</param>
     /// <param name="height">The height the font should have</param>
+    /// <remarks>
+    /// Keep in mind that some characters might not work in this font.<br/>
+    /// I would recommend to test if the character is included.
+    /// </remarks>
     /// <returns><see cref="NexusFont"/></returns>
     public static NexusFont Terminal(int width, int height)
         => new("Terminal", width, height);
