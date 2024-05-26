46d4100b560ae5dee13c4e6bf0ef5c99ba55ea51
Leon Schimmel <leonschimmel15@gmail.com>
2024-05-26 14:32:05 +0200

    Added helper methods to check if the console window is conhost.exe and allows to restart the program in a conhost.exe process

---
+++ Helpers/NexusEngineHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleNexusEngine.Helpers;
+
+using System.Diagnostics;
+using System.IO;
+
+/// <summary>
+/// Useful helper methods for <see cref="ConsoleNexusEngine"/>
+/// </summary>
+public static class NexusEngineHelper
+{
+    /// <summary>
+    /// <see langword="true"/> if the console window is a conhost.exe process, otherwise <see langword="false"/>
+    /// </summary>
+    /// <returns><see cref="bool"/></returns>
+    public static bool IsConsoleHost() => Native.GetWindowLong(Native.GetConsoleWindow(), -16) > 0;
+
+    /// <summary>
+    /// Starts the program in a new conhost.exe window
+    /// </summary>
+    public static void StartAppInConhost()
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = Path.Combine(Environment.SystemDirectory, "conhost.exe"),
+            Arguments = Environment.ProcessPath ?? throw new FileNotFoundException("The .exe path of the process couldn't be found"),
+            UseShellExecute = false
+        });
+    }
+}
